e5b0ea9ac0b247d1846269e2f8ac01e0eeb91d51
Tyuiu.KozhevnikovDG@outlook.com <f1zog2005@mail.ru>
2023-09-09 00:37:43 +0500

    Task4

---
+++ Tyuiu.KozhevnikovDG.Sprint0.Task4.V0.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.KozhevnikovDG.Sprint0.Task4.V0.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.KozhevnikovDG.Sprint0.Task4.V0.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("9a9e1a2a-e0bb-40c5-8f37-ebf2fe1e90b7")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Tyuiu.KozhevnikovDG.Sprint0.Task4.V0/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Tyuiu.KozhevnikovDG.Sprint0.Task4.V0.Lib;
+
+namespace Tyuiu.KozhevnikovDG.Sprint0.Task4.V0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Пример линейной структуры 
+            Console.WriteLine(DataService.Addition(1, 5));
+
+            Console.WriteLine(DataService.Subtraction(15, 5));
+
+            Console.WriteLine(DataService.Multiplication(24, 45));
+
+            Console.WriteLine(DataService.Division(14, 7));
+
+            Console.ReadKey();
+        }
+    }
+}
