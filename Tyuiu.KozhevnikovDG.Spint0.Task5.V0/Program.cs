ef40a746a1d5b58436aecccecf4397b27331f81e
Tyuiu.KozhevnikovDG@outlook.com <f1zog2005@mail.ru>
2023-09-11 22:13:05 +0500

    Task5

---
+++ Tyuiu.KozhevnikovDG.Spint0.Task5.V0/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Tyuiu.KozhevnikovDG.Sprint0.Task5.V0.Lib;
+
+namespace Tyuiu.KozhevnikovDG.Sprint0.Task4.V0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Применение линейной структуры
+            Console.WriteLine("A + B =" + DataService.Addition(5, 5));
+            Console.WriteLine("A - B =" + DataService.Subtraction(10, 5));
+            Console.WriteLine("A * B =" + DataService.Multiplication(10, 5));
+            Console.WriteLine("A / B =" + DataService.Division(9, 3));
+            Console.ReadKey();
+        }
+    }
+}
+
+
