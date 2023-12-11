ce3818ab651dc8bcf0828c05760bb76a62af930e
Tyuiu.KozhevnikovDG@outlook.com <f1zog2005@mail.ru>
2023-12-11 19:41:36 +0500

    TaskReview

---
+++ Tyuiu.KozhevnikovDG.Sprint0.TaskReview.V0.Test/DataServicecTest.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Tyuiu.KozhevnikovDG.Sprint0.TaskReview.V0.Lid;
+
+namespace Tyuiu.KozhevnikovDG.Sprint0.TaskReview.V0.Test
+{
+    [TestClass]
+    public class DataServicecTest
+    {
+        [TestMethod]
+        public void Calculate()
+        {
+            Assert.AreEqual(30, DataService.Calc(1, 2, 3));
+        }
+    }
+}
+    
+
